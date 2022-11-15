c4b30b42a835b03ca3c5cd2f1e080ace389ca7ef
Sébastien Gallou <sebastien.gallou@gmail.com>
2022-11-15 22:30:33 +0100

    Fix plugin mock

--- tools/mocks/plugins.js
+++ tools/mocks/plugins.js
@@ -5,8 +5,8 @@ const plugins = JSON.stringify({
       version: '1.0.2',
       author: 'Oussama DAHMAZ',
       url: '',
-      'support-manually-created-device': false,
-      'support-device-removed-notification': false,
+      'supportManuallyCreatedDevice': false,
+      'supportDeviceRemovedNotification': false,
       package: {
         type: 'LametricTime',
         version: '1.0.2',
@@ -70,8 +70,8 @@ const plugins = JSON.stringify({
       version: '1.2.0',
       author: 'Jean-Michel BERHAULT',
       url: 'https://github.com/Yadoms/yadoms/wiki/SystemInformation',
-      'support-manually-created-device': false,
-      'support-device-removed-notification': false,
+      'supportManuallyCreatedDevice': false,
+      'supportDeviceRemovedNotification': false,
       package: {
         type: 'SystemInformation',
         version: '1.2.0',
@@ -92,8 +92,8 @@ const plugins = JSON.stringify({
       version: '2.1.0',
       author: 'Sébastien Gallou',
       url: 'https://github.com/Yadoms/yadoms/wiki/EnOcean',
-      'support-manually-created-device': false,
-      'support-device-removed-notification': true,
+      'supportManuallyCreatedDevice': false,
+      'supportDeviceRemovedNotification': true,
       package: {
         type: 'enOcean',
         version: '2.1.0',
@@ -1980,8 +1980,8 @@ const plugins = JSON.stringify({
       version: '1.3.1',
       author: 'Sébastien GALLOU',
       url: 'https://github.com/Yadoms/yadoms/wiki/Rfxcom',
-      'support-manually-created-device': true,
-      'support-device-removed-notification': false,
+      'supportManuallyCreatedDevice': true,
+      'supportDeviceRemovedNotification': false,
       package: {
         type: 'rfxcom',
         version: '1.3.1',
