2e3e9e880fdd201e6b66faaf95b27daf4e050837
oussama Dahmaz <oussama.dahmaz@gmail.com>
2023-04-12 19:18:57 +0200

    fix store key

--- libs/store/src/lib/store.ts
+++ libs/store/src/lib/store.ts
@@ -13,7 +13,7 @@ export const store = configureStore({
   },
   // Additional middleware can be passed to this array
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-  devTools: process.env['NODE_ENV'] !== 'production',
+  devTools: process.env.NODE_ENV !== 'production',
   // Optional Redux store enhancers
   enhancers: [],
 });
