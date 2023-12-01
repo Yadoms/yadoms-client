e09cd67b721b74035ce8b94ee03416a5140d5107
Sébastien Gallou <sebastien.gallou@gmail.com>
2023-12-01 19:04:12 +0100

    WS : Manage device creation/deletion

--- libs/shared/src/lib/services/YadomsWebSocketConnection.tsx
+++ libs/shared/src/lib/services/YadomsWebSocketConnection.tsx
@@ -6,6 +6,17 @@ export interface Acquisition {
   value: string;
 }
 
+export interface Device {
+  id: number;
+  pluginId: number;
+  friendlyName: string;
+  model: string;
+  details: object;
+  configuration: object;
+  type: string;
+  blacklist: boolean;
+}
+
 type AcquisitionListeners = (newAcquisition: Acquisition) => void;
 
 class YadomsWebSocketConnection {
@@ -36,14 +47,27 @@ class YadomsWebSocketConnection {
       }
       if ("newAcquisition" in data) {
         const newAcquisition: Acquisition = {
-          date: this.parseYadomsDate(data.newAcquisition.date), keyword: data.newAcquisition.keywordId, value: data.newAcquisition.value
+          date: this.parseYadomsDate(data.newAcquisition.date),
+          keyword: data.newAcquisition.keywordId,
+          value: data.newAcquisition.value
         };
         this._acquisitionsListeners.get(newAcquisition.keyword)?.forEach((listener) => {
           listener(newAcquisition)
         })
         return;
       }
-      console.warn("Unknown socket message received : " + event);
+      if ("deviceCreated" in data) {
+        const newDevice: Device = this.parseDevice(data.deviceCreated);
+        console.log("WS > Device created : " + newDevice.friendlyName); // TODO Just log for now
+        return;
+      }
+      if ("deviceDeleted" in data) {
+        const newDevice: Device = this.parseDevice(data.deviceDeleted);
+        console.log("WS > Device deleted : " + newDevice.friendlyName); // TODO Just log for now
+        return;
+      }
+      //TODO keywordCreated, keywordDeleted
+      console.warn("WS > Unknown socket message received : " + event);
     }
   }
 
@@ -72,6 +96,19 @@ class YadomsWebSocketConnection {
 
   private parseYadomsDate(dateAsString: string): Date {
     return new Date(dateAsString.replace(/([0-9]{4})([0-9]{2})([0-9]{2})T([0-9]{2})([0-9]{2})([0-9]{2}).([0-9]*)/, "$1-$2-$3T$4:$5:$6.$7"));
+  }
+
+  private parseDevice(deviceAsJson: any): Device {
+    return {
+      id: deviceAsJson.Id,
+      pluginId: deviceAsJson.PluginId,
+      friendlyName: deviceAsJson.FriendlyName,
+      model: deviceAsJson.Model,
+      details: deviceAsJson.Details,
+      configuration: deviceAsJson.Configuration,
+      type: deviceAsJson.Type,
+      blacklist: deviceAsJson.Blacklist
+    };
   }
 
   onConnected: ((connected: boolean) => void) | undefined;
