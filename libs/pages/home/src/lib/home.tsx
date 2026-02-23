713d8112540cd0b4a5ac9145fc20a159955aab2b
Sébastien Gallou <sebastien.gallou@gmail.com>
2026-02-23 19:33:53 +0100

    Add grid

--- libs/pages/home/src/lib/home.tsx
+++ libs/pages/home/src/lib/home.tsx
@@ -1,7 +1,9 @@
 import { useContext } from 'react';
 import { YadomsConnectionContext, YadomsConnection } from '@yadoms/shared';
-import YButton from './widgets/YButton';
+import YButton from './widgets/ybutton';
 import KeywordLog from './widgets/keywordLog';
+import classes from './home.module.css';
+import classNames from "classnames";
 
 /* eslint-disable-next-line */
 export interface PagesHomeProps {}
@@ -16,10 +18,24 @@ export function Home(props: PagesHomeProps) {
       <h1>Welcome to PagesHome!</h1>
       <p>Socket is {connected ? 'connected' : 'DISCONNECTED'}</p>
       <p>Server current time is {serverCurrentTime?.toString()}</p>
-      <YButton widgetId={1} buttonKeyword={20}></YButton>
-      <YButton widgetId={2} buttonKeyword={30}></YButton>
-      <KeywordLog widgetId={3} keywordsToListen={'45,46'}></KeywordLog>
-      <KeywordLog widgetId={4} keywordsToListen={'24, 25, 26'}></KeywordLog>
+      <div className={classes.grid}>
+        
+        <div className={classNames(classes.card, classes.tile)}>
+          <YButton widgetId={1} buttonKeyword={20}></YButton>
+        </div>
+
+        <div className={classNames(classes.card, classes.tile)}>
+          <YButton widgetId={2} buttonKeyword={30}></YButton>
+        </div>
+
+        <div className={classNames(classes.card, classes.wide)}>
+          <KeywordLog widgetId={3} keywordsToListen={'45,46'}></KeywordLog>
+        </div>
+
+        <div className={classNames(classes.card, classes.wide)}>
+          <KeywordLog widgetId={4} keywordsToListen={'24, 25, 26'}></KeywordLog>
+        </div>
+      </div>
     </div>
   );
 }
